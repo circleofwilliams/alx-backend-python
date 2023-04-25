c71546809d9df3b2359f2ca052e76734755cd09f
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-04-25 20:38:02 +0000

    unit and integration test update

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -1,8 +1,8 @@
 # TESTING (UNIT AND INTEGRATION)
-
 This folder contains files that answers task questions on unit and integration testing in python:
 
 concepts tested includes:
-  1. The difference between unit and integration tests using python.
-  2. Using the mock class from the unittest to have predetermined actions and activities of some functions. 
-  3. Memoization: Dynamic programming
+
+The difference between unit and integration tests using python.
+Using the mock class from the unittest to have predetermined actions and activities of some functions.
+Memoization: Dynamic programming
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,23 +7,28 @@ from unittest.mock import patch
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """An access_nested_map function test class"""
     @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-#    def test_access_nested_map(self, nested_map, path, expected):
-#        """Test to access nested map with key path.
-#        Parameters
-#        ----------
-#            nested_map: A nested map
-#            path: A sequence of key representing a path to the value
-#            expected: Expected result from the function
-#        """
-#        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test to access nested map with key path.
+        Parameters
+        ----------
+            nested_map: A nested map
+            path: A sequence of key representing a path to the value
+            expected: Expected result from the function
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
 
     def test_access_nested_map_exception(self, nested_map, path):
         """Test for an Exception to access nested map with key path.
@@ -45,7 +50,7 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, expected):
-        """Test for an Exception to access nested map with key path.
+        """Test for get_json function with test url.
         Parameters
         ----------
             test_url: A test url
@@ -56,3 +61,32 @@ class TestGetJson(unittest.TestCase):
             res = get_json(test_url)
             self.assertEqual(res, expected)
             mock.get.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """A memoize function test class
+    """
+    @parameterized.expand([
+        (42, ),
+        (42, ),
+    ])
+    def test_memoize(self, expected):
+        """Test for an Exception to access nested map with key path.
+        Parameters
+        ----------
+            test_url: A test url
+            expected: Expected result from the function
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch('TestClass.a_method') as mock:
+            res = [TestClass.a_property(), TestClass.a_property()]
+            self.assertEqual(res, result)
+            mock.assert_called_once()
